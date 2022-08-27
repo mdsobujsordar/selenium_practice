00cbd9b101e054b9f626faa497c374f82df6f2cf
Md Sobuj Sordar <mdsobujsordar@Mds-MacBook-Air.local>
2022-08-27 07:38:19 -0700

    registration & login testcase

--- src/test/java/testCasePackage/TC_001LoginPage.java
+++ src/test/java/testCasePackage/TC_001LoginPage.java
@@ -7,22 +7,41 @@ import driverPackage.baseDriver;
 
 public class TC_001LoginPage extends baseDriver {
 	String baseURL = "https://practice.automationtesting.in/my-account/";
-	@Test
-	public void locatorLearing() throws InterruptedException {
+	@Test(priority=1)
+	public void Registration() throws InterruptedException {
 		driver.get(baseURL);
 		Thread.sleep(2000);
 		driver.manage().window().maximize();
 		
-		driver.findElement(By.id("username")).sendKeys("sobuz");
+		driver.findElement(By.id("reg_email")).sendKeys("abcd4432@gmail.com");
 		Thread.sleep(2000);
 		
-		driver.findElement(By.id("password")).sendKeys("sobuz");
+		driver.findElement(By.id("reg_password")).sendKeys("Abcd@#4432");
 		Thread.sleep(2000);
 		
-		driver.findElement(By.xpath("//body/div[@id='pagewrap']/div[@id='body']/div[@id='layout']/div[@id='content']/div[@id='page-36']/div[1]/div[1]/div[1]/div[1]/form[1]/p[3]/input[3]")).click();
+		//Custom Xpath
+		driver.findElement(By.xpath("//input[@value='Register']")).click();
 		Thread.sleep(2000);
 		
-		System.out.println(driver.findElement(By.xpath("//body/div[@id='pagewrap']/div[@id='body']/div[@id='layout']/div[@id='content']/div[@id='page-36']/div[1]/div[1]/ul[1]/li[1]")).getText());
+		//Copied Xpath
+		String text= driver.findElement(By.xpath("//*[@id=\"page-36\"]/div/div[1]/ul")).getText();
 		Thread.sleep(2000);
+		
+		System.out.println("Registration status: "+ text);
+	}
+	@Test(priority=2)
+	public void login() throws InterruptedException {
+		driver.findElement(By.id("username")).sendKeys("abcd4432@gmail.com");
+		Thread.sleep(2000);
+		
+		driver.findElement(By.id("password")).sendKeys("Abcd@#4432");
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//input[@value='Login']")).click();
+		
+		String text = driver.findElement(By.xpath("//a[@href='https://practice.automationtesting.in/my-account/customer-logout/']")).getText();
+		Thread.sleep(2000);
+		
+		System.out.println("Login Confirmation: "+ text);
 	}
 }
